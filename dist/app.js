6e30ef2afbf18d0d5de107f3bb02a16cd815d363
Natale Capristo <andreacapristo1@gmail.com>
2021-04-27 21:37:55 +0200

    header

--- dist/app.js
+++ dist/app.js
@@ -10,6 +10,15 @@
 function initVue() {
   new Vue({
     el: '#app',
+    data: {
+      navbar: ['Home', 'About', 'Service', 'Blog', 'Contact', 'Portfolio', 'Sign In'] // whatDoweDO:[
+      //   {
+      //     img:,
+      //     title:
+      //   }
+      // ]
+
+    },
     methods: {}
   });
 }
--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
+
+
 function initVue(){
 
   new Vue ({
 
     el:'#app',
 
+    data:{
+      navbar:['Home','About','Service', 'Blog', 'Contact', 'Portfolio', 'Sign In'],
+      // whatDoweDO:[
+      //   {
+      //     img:,
+      //     title:
+      //   }
+      // ]
+
+    },
+
     methods:{
 
-      
+
     },
+
   });
 }
 
