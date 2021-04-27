f86905b24fa58b9ce5ba058b84a276b303cd5eff
Natale Capristo <andreacapristo1@gmail.com>
2021-04-27 13:13:40 +0200

    Project basic structure

---
+++ src/app.js
@@ -0,0 +1,20 @@
+function initVue(){
+
+  new Vue ({
+
+    el:'#app',
+
+    methods:{
+
+      
+    },
+  });
+}
+
+
+
+function init(){
+  initVue();
+}
+
+document.addEventListener('DOMContentLoaded', init);
